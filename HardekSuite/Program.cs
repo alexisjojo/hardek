bda87b8f59ad75151a6160b86df1eab63c7aea6b
hardeke <hardeke@b8d110b4-8d25-11de-8988-25e8a898b690>
2009-09-01 23:53:24 +0000

    Switched to normal ClientChooser;

--- HardekSuite/Program.cs
+++ HardekSuite/Program.cs
@@ -13,7 +13,7 @@ namespace HardekSuite {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Core = new Kedrah.Core("Hardek", "Hardek", true);
+            Core = new Kedrah.Core("Hardek", "Hardek", false);
 
             if (Core.Client == null)
                 Environment.Exit(0);
